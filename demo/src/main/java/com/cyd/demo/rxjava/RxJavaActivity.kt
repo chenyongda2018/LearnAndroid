9a54f10a31874204682792c2fb027a2ddb3f5e3c
chenyongda <chenchenyongda@gmail.com>
2022-06-08 23:18:09 +0800

    rxjava

--- demo/src/main/java/com/cyd/demo/rxjava/RxJavaActivity.kt
+++ demo/src/main/java/com/cyd/demo/rxjava/RxJavaActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.cyd.demo.R
+import com.cyd.demo.databinding.ActivityRxJavaBinding
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 
 class RxJavaActivity : AppCompatActivity() {
@@ -16,19 +18,26 @@ class RxJavaActivity : AppCompatActivity() {
 
     var mDisposable: Disposable? = null
 
+    private var _binding: ActivityRxJavaBinding? = null
+    private val binding get()= _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rx_java)
-        test1()
+        _binding = ActivityRxJavaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        test1()
+//        test2()
+//        test3()
     }
 
+    /**
+     * Just
+     */
     private fun test1() {
-        val observable = Observable.fromArray(
+        val observable = Observable.just(
             "Ant", "Ape",
             "Bat", "Bee", "Bear", "Butterfly",
-            "Cat", "Crab", "Cod",
-            "Dog", "Dove",
-            "Fox", "Frog"
+            "Cat", "Crab", "Cod"
         )
         val observer = object : Observer<String> {
             override fun onSubscribe(d: Disposable) {
@@ -56,6 +65,63 @@ class RxJavaActivity : AppCompatActivity() {
                 str.lowercase().startsWith("b")
             }
             .subscribe(observer)
+    }
+
+    /**
+     * range
+     */
+    private fun test2() {
+        Observable.range(1, 20)
+//        Observable.fromArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//            11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .filter { it % 2 == 0 }
+            .map { "$it is even num" }
+            .subscribe(object : DisposableObserver<String>() {
+                override fun onNext(t: String) {
+                    Log.d(TAG, "Number: $t")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+                override fun onComplete() {
+                    Log.d(TAG, "Number: onComplete")
+                }
+            })
+    }
+
+    /**
+     * Buffer
+     * Buffer gathers items emitted by an Observable into batches and emit the batch instead of emitting one item at a time.
+     * 将数据集合，n个一组发送
+     */
+    private fun test3() {
+        Observable.just(1, 2, 3, 4,
+            5, 6, 7, 8, 9)
+            .buffer(3)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object: Observer<List<Int>> {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: List<Int>) {
+                    Log.d(TAG,"onNext,${t.size}")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+                override fun onComplete() {
+                }
+
+            })
+    }
+
+    private fun testRxView() {
+        RxView
     }
 
     override fun onDestroy() {
